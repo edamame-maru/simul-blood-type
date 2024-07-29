1438b7feef29e48de25d5bb0dc1a861a177cd9b2
edamame-maru <so1maru.ueda@gmail.com>
2024-07-29 15:29:38 +0900

    tried valiantly but failed

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
     /* Blood types are as follows;
     AA
@@ -12,14 +12,51 @@ int main()
     */
 
     int population, cycles = 0;
+    double nAA, nAO, nBB, nBO, nAB, nOO = 0.0;
     
     std::cout << "Enter population: ";
-    std::cin >> population; 
+    std::cin >> population;
+    std::cout << "Population: " << population << std::endl;
 
     std::cout << "Enter cycles: ";
-    std::cin >> cycles; 
-
-    std::cout << "Population: " << population << std::endl;
+    std::cin >> cycles;
     std::cout << "Cycles: " << cycles << std::endl;
+
+    std::cout << "Enter nAO: " << std::endl;
+    std::cin >> nAO;
+    std::cout << "nAO: " << std::endl;
+
+    std::cout << "Enter nAA: " << std::endl;
+    std::cin >> nAA;
+    std::cout << "nAA: " << std::endl;
+
+    std::cout << "Enter nBO: " << std::endl;
+    std::cin >> nBO;
+    std::cout << "nBO: " << std::endl;
+
+    std::cout << "Enter nBB: " << std::endl;
+    std::cin >> nBB;
+    std::cout << "nBB: " << std::endl;
+
+    std::cout << "Enter nAB: " << std::endl;
+    std::cin >> nAB;
+    std::cout << "nAB: " << std::endl;
+
+    std::cout << "Enter nOO: " << std::endl;
+    std::cin >> nOO;
+    std::cout << "nOO: " << std::endl;
+
+
+    int samples[population];
+
+    for (int i = 0; i < population; ++i)
+    {
+        
+    }
+
+    for (int i = 0; i < cycles; ++i)
+    {
+        
+    }
     return 0;
 }
